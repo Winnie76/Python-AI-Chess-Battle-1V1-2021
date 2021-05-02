bc1946fca2b9233a904a82af6e1a82dc6c294c48
Estherrrrb <80498731+Estherrrrb@users.noreply.github.com>
2021-05-03 01:51:41 +0800

    without pruning的大概框架

--- team_name/player.py
+++ team_name/player.py
@@ -34,6 +34,36 @@ class Player:
         of the game, select an action to play this turn.
         """
         # put your code here
+        //在state里找出所有player token； 所有opponent token
+        state=self.state
+        player_list=token_list(current_state, "player")
+        opponent_list=token_list(current_state, "opponent")
+        max1_layer=[]
+        for item in player_list: #max level
+            ol = possible_move(current_state, item)
+            min1_layer=[]
+            for opp in opponent_list; #min_level
+                opp_ol = possible_move(current_state, item)
+                max2_layer=[]
+                for action in ol: #max_level
+                    min2_layer=[]
+                    for opp_action in opp_ol: #min_level
+                        current_state=update(self, action, opp_action) #如果再来一轮 ，应该从这里加入
+                        eval=evaluate(current_state)
+                        min2_layer.append(eval)
+                        self.state = state
+                    max2_layer.append(min_layer)
+                min1_layer.append(max2_layer)
+            max1_layer.append(min1_layer)
+
+        #[[[[1,2],[2,3]],[[2,3],[1,3]]],[[[1,2],[2,3]],[[2,3],[1,3]]]]
+        # 加入pruning
+        # 加入后续判断
+
+
+
+
+
     
     def update(self, opponent_action, player_action):
         """
@@ -129,15 +159,18 @@ class Player:
                     # if this token lose, then take this token away from the state
                     pass
 
-def if_defeat(new,old):
-    if (new == 's') & (old == 'p'):
-        return 1
+def if_defeat(new, old):
+    if (new == old):
+        return "DRAW"
     elif (new == 'r') & (old == 's'):
-        return 1
+        return "WIN"
     elif (new == 'p') & (old == 'r'):
-        return 1
+        return "WIN"
+    elif (new == 's') & (old == 'p'):
+        return "WIN"
     else:
-        return 0
+        return "LOSE"
+
 
 def board_range():
     v1 = [0, -5]
@@ -178,3 +211,116 @@ def board_range():
         r_q = ['Block', r + i, q - i]
         range_limit.append(r_q)
     return (range_limit)
+
+def possible_move(state, token):
+    ol = []
+    layer1 = six_hex_surrond(token)
+    for surround_item in layer1:
+        # surround_item is [1,2]   upper_current_pos is [x, y]
+        ol = if_ol_append(state, surround_item, upper_current_pos, ol)
+        #print("upper_current",upper_current_pos)
+        #print("state",state)
+        ...
+    return ol #每一个action和update中action格式一样
+
+def func_open_list(state, upper_current_pos, target, list_no_cost):
+    # upper_current_pos means upper upper_current_pos
+    #possible_random_move = func_open_list(
+       # state, list(upper_tuple[1:3]), ['r', 1, 1], 1)
+    ol = []
+    ol_with_cost = []
+    # six hex connected to the upper_current_pos
+    layer1 = six_hex_surrond(upper_current_pos)
+    #if upper_current_pos not in state:
+    # slide for all possible surrounding hexes
+    #print(state)
+    #print(upper_current_pos)
+    #print(layer1)
+    for surround_item in layer1:
+        # surround_item is [1,2]   upper_current_pos is [x, y]
+        ol = if_ol_append(state, surround_item, upper_current_pos, ol)
+        #print("upper_current",upper_current_pos)
+        #print("state",state)
+    print(ol)
+    # swing
+    for i in range(6):
+        surround_item = layer1[i]
+        if (tuple(surround_item) in state):
+            # have upper upper_current_pos --> can swing
+            if (state[tuple(surround_item)].isupper()):
+                # six hex connected to the upper upper_current_pos for swing
+                layer2 = six_hex_surrond(surround_item)
+                for j in range(i-1, i+2, 1):  # three hex opposite side
+                    if (j == 6):  # the hex next to no.5 in the clockwise list is no.0 and no.4
+                        ol = if_ol_append(
+                            state, layer2[0], upper_current_pos, ol)
+                    else:
+                        ol = if_ol_append(
+                            state, layer2[j], upper_current_pos, ol)
+    if list_no_cost == 1:
+        return ol
+    for movable_hex in ol:
+        movable_hex = list(movable_hex)
+        # print('movable_hex', movable_hex, 'target', target)
+        cost = func_upper_lower_distance(movable_hex, target)
+        movable_hex.append(cost)
+        ol_with_cost.append(movable_hex)
+
+    return ol_with_cost
+
+def if_ol_append(state, item, token, ol):  # check if the item should be added to open list
+
+    new_ol = [i[0:2] for i in ol]
+
+    if item not in new_ol:  # to avoid double record
+        if tuple(item) in state:
+            t1 = [state[tuple(token)]]+[token]
+            #print("ut",ut)
+            t2 = [state[tuple(item)]]+[item]
+            #print("lt",lt)
+            # append if not block or undefeatable lower token (upper, lower) uppper couldn't defeat lower --> 0
+            if not (if_defeat(t1, t2) == 0):
+                ol.append(item)
+        else:
+            ol.append(item)
+    else:
+        return ol
+    return ol
+
+# return list of locations [[1,2], [2,5], [-1,]), [0,6], [9,3], [3,4]]
+def six_hex_surrond(token):  # find the six surronding hex for a given hex
+    if len(token) == 3:
+        token = token[1:3]
+    action = [[-1, 0], [0, -1], [1, -1], [1, 0], [0, 1],
+              [-1, 1]]  # six direction list in clockwise order
+    six_hex = []
+    for item in action:
+        x = item[0]+token[0]
+        y = item[1]+token[1]
+        loc = [x, y]  # coordinate of the hex
+        six_hex.append(loc)
+    return six_hex
+
+def evaluate(state):
+
+
+def least_distance(t1, t2):
+
+
+
+def token_list(state, side): #side "player"/"opponent"
+    token_list={}
+    for key in state:
+        if item in state[key]:
+            if len(item) > 1:
+                for i in item:
+                    if state[key][0] == side:
+                        j = {key: state[key][1]}
+                        token_list.update(j)
+
+            elif len(item) == 1:
+                if item != 'Block':
+                    if state[key][0] == side:
+                        j = {key: state[key][1]}
+                        token_list.update(j)
+    return token_list
